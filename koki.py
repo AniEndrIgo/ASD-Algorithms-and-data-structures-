ec8cd583cf7e38d57c4980eb6b54df0d13af4ab9
aniendrigo <androsov.endrigo75@mail.ru>
2026-09-25 11:16:20 +0200

    oop

--- koki.py
+++ koki.py
@@ -1,150 +1,132 @@
 import math
-from typing import Union
-
-Number = Union[int, float]
 
 
 class Angle:
     TAU = 2.0 * math.pi
 
-    def __init__(self, radians: Number = 0.0) -> None:
+    def __init__(self, radians=0.0):
         self.set_radians(radians)
 
-    # ---------- фабричные методы ----------
     @classmethod
-    def from_radians(cls, radians: Number) -> "Angle":
+    def from_radians(cls, radians):
         return cls(radians)
-
     @classmethod
-    def from_degrees(cls, degrees: Number) -> "Angle":
+    def from_degrees(cls, degrees):
         return cls(math.radians(float(degrees)))
 
-    # ---------- геттеры/сеттеры без @property ----------
-    def get_radians(self) -> float:
+    def get_radians(self):
         return self._radians
-
-    def set_radians(self, radians: Number) -> None:
+    def set_radians(self, radians):
         if not isinstance(radians, (int, float)):
-            raise TypeError("radians must be int or float")
+            raise TypeError("!!")
         self._radians = float(radians)
 
-    def get_degrees(self) -> float:
+    def get_degrees(self):
         return math.degrees(self._radians)
-
-    def set_degrees(self, degrees: Number) -> None:
+    def set_degrees(self, degrees):
         if not isinstance(degrees, (int, float)):
-            raise TypeError("degrees must be int or float")
+            raise TypeError("!!")
         self._radians = math.radians(float(degrees))
 
-    # ---------- приведение к каноническому виду [0, 2π) ----------
-    def _normalized(self) -> float:
+    def _normalized(self):
         return self._radians % self.TAU
 
-    def _eq(self, other: "Angle") -> bool:
+    def __eq__(self, other):
+        if not isinstance(other, Angle):
+            return NotImplemented
         diff = (self._radians - other._radians) % self.TAU
         return (
             math.isclose(diff, 0.0, abs_tol=1e-9)
             or math.isclose(diff, self.TAU, abs_tol=1e-9)
-        )
+    )
 
-    # ---------- сравнение ----------
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, Angle):
-            return NotImplemented
-        return self._eq(other)
-
-    def __ne__(self, other: object) -> bool:
+    def __ne__(self, other):
         result = self.__eq__(other)
         if result is NotImplemented:
             return NotImplemented
         return not result
 
-    def __lt__(self, other: "Angle") -> bool:
+    def __lt__(self, other):
         if not isinstance(other, Angle):
             return NotImplemented
         if self == other:
             return False
         return self._normalized() < other._normalized()
 
-    def __le__(self, other: "Angle") -> bool:
+    def __le__(self, other):
         if not isinstance(other, Angle):
             return NotImplemented
         if self == other:
             return True
         return self._normalized() < other._normalized()
 
-    def __gt__(self, other: "Angle") -> bool:
+    def __gt__(self, other):
         if not isinstance(other, Angle):
             return NotImplemented
         if self == other:
             return False
         return self._normalized() > other._normalized()
 
-    def __ge__(self, other: "Angle") -> bool:
+    def __ge__(self, other):
         if not isinstance(other, Angle):
             return NotImplemented
         if self == other:
             return True
         return self._normalized() > other._normalized()
 
-    # ---------- преобразование к float и int ----------
-    def __float__(self) -> float:
+    def __float__(self):
         return self._radians
 
-    def __int__(self) -> int:
+    def __int__(self):
         return int(self._radians)
 
-    # ---------- сложение ----------
-    def __add__(self, other: Union["Angle", Number]) -> "Angle":
+    def __add__(self, other):
         if isinstance(other, Angle):
             return Angle.from_radians(self._radians + other._radians)
         if isinstance(other, (int, float)):
             return Angle.from_radians(self._radians + other)
         return NotImplemented
 
-    def __radd__(self, other: Number) -> "Angle":
+    def __radd__(self, other):
         if isinstance(other, (int, float)):
             return Angle.from_radians(other + self._radians)
         return NotImplemented
 
-    # ---------- вычитание ----------
-    def __sub__(self, other: Union["Angle", Number]) -> "Angle":
+    def __sub__(self, other):
         if isinstance(other, Angle):
             return Angle.from_radians(self._radians - other._radians)
         if isinstance(other, (int, float)):
             return Angle.from_radians(self._radians - other)
         return NotImplemented
 
-    def __rsub__(self, other: Number) -> "Angle":
+    def __rsub__(self, other):
         if isinstance(other, (int, float)):
             return Angle.from_radians(other - self._radians)
         return NotImplemented
 
-    # ---------- умножение на число ----------
-    def __mul__(self, other: Number) -> "Angle":
+    def __mul__(self, other):
         if isinstance(other, (int, float)):
             return Angle.from_radians(self._radians * other)
         return NotImplemented
 
-    def __rmul__(self, other: Number) -> "Angle":
+    def __rmul__(self, other):
         if isinstance(other, (int, float)):
             return Angle.from_radians(other * self._radians)
         return NotImplemented
 
-    # ---------- деление на число ----------
-    def __truediv__(self, other: Number) -> "Angle":
+    def __truediv__(self, other):
         if isinstance(other, (int, float)):
             return Angle.from_radians(self._radians / other)
         return NotImplemented
 
-    # ---------- дополнительно ----------
-    def __neg__(self) -> "Angle":
+    # ---------- прочее ----------
+    def __neg__(self):
         return Angle.from_radians(-self._radians)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"{self.get_degrees():.6g}° ({self._radians:.6g} rad)"
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"Angle.from_radians({self._radians!r})"
 
 
@@ -154,16 +136,15 @@ if __name__ == "__main__":
 
     print(a)
     print(repr(a))
-
-    print(a == b)                 # True
-    print(a.get_degrees())        # 30.0
-    print(a.get_radians())        # 0.5235987755982988
+    print(a == b)
+    print(a.get_degrees())
+    print(a.get_radians())
 
     a.set_degrees(90)
     print(a.get_degrees(), a.get_radians())
 
-    print(Angle.from_degrees(370) == Angle.from_degrees(10))  # True
-    print(Angle.from_degrees(10) < Angle.from_degrees(20))    # True
+    print(Angle.from_degrees(370) == Angle.from_degrees(10))
+    print(Angle.from_degrees(10) < Angle.from_degrees(20))
 
     print(a + b)
     print(a - b)
@@ -174,3 +155,5 @@ if __name__ == "__main__":
     print(a * 2)
     print(2 * a)
     print(a / 2)
+    print(-a)
+    print(float(a), int(a))
